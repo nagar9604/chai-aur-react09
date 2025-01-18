153c080b8ae18e0057bb8f4deb7a3021b45c05bb
shruti <nagarshruti96@gmail.com>
2025-01-18 14:44:13 +0530

    react

---
+++ 07project/src/Components-1/User/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+function User() {
+  const {userid}= useParams()
+  return (
+    <div className='bg-gray-400 text-pink text-3xl p-4'>User:{userid}</div>
+  )
+}
+
+export default User
